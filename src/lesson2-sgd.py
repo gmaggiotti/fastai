2a4edb97dbfbb97ef9f47296b83c6e58bd1252ed
gabe <gmaggiotti@gmail.com>
2019-10-12 10:22:36 -0300

    simple lineal model with sdg in torch

---
+++ src/lesson2-sgd.py
@@ -0,0 +1,28 @@
+from fastai.basics import *
+from matplotlib.pyplot import figure
+import matplotlib.pyplot as plt
+
+n = 100
+x = torch.ones(n,2)
+x[:,0].uniform_(-1.,1)
+a = tensor([3.,2])
+y = x@a + torch.rand(n)
+
+def mse(y_hat, y): return ((y_hat-y)**2).mean()
+a = nn.Parameter(a)
+
+def update():
+    y_hat = x@a
+    loss = mse(y, y_hat)
+    if t % 10 == 0: print(loss)
+    loss.backward()
+    with torch.no_grad():
+        a.sub_(lr * a.grad)
+        a.grad.zero_()
+
+lr = 1e-1
+for t in range(100): update()
+
+plt.scatter(x[:,0],y)
+plt.scatter(x[:,0],x@a);
+plt.show()
