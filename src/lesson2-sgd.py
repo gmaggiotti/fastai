bdf412388a3b74b9d5e99e4c36fe0bf7b84aa1e5
gabe <gmaggiotti@gmail.com>
2019-10-12 13:27:57 -0300

    adding a cuadratic regresion using pytorch

--- src/lesson2-sgd.py
+++ src/lesson2-sgd.py
@@ -5,23 +5,24 @@ n = 100
 x = torch.ones(n,2)
 x[:,0].uniform_(-1.,1)
 a = tensor([3.,2])
-y = x@a + torch.rand(n)
+y = x**2@a + torch.rand(n)
 
 def rmse(y_hat, y): return ((y_hat - y) ** 2).mean()
 a = nn.Parameter(a)
 
 def update():
-    y_hat = x@a
+    y_hat = x**2@a
     loss = rmse(y, y_hat)
     if t % 10 == 0: print(loss)
     loss.backward()
     with torch.no_grad():
+        # Calculate de grad of loss using lr and substract them from a
         a.sub_(lr * a.grad)
         a.grad.zero_()
 
 lr = 1e-1
-for t in range(100): update()
+for t in range(1000): update()
 
 plt.scatter(x[:,0],y)
-plt.scatter(x[:,0],x@a);
+plt.scatter(x[:,0],x**2@a);
 plt.show()
